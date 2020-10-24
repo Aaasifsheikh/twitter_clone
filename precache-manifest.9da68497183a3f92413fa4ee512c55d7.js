faa76483834963d60df2dac53a1f7716618668eb
Aasif Sheikh <Aasifsheikh820@gmail.com>
2020-10-24 09:32:03 +0530

    Updates

---
+++ precache-manifest.9da68497183a3f92413fa4ee512c55d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317db422f78f69fe2766ce0447c28214",
+    "url": "/twitter_clone/index.html"
+  },
+  {
+    "revision": "43efb1d0e23f45644511",
+    "url": "/twitter_clone/static/css/main.d9abf41c.chunk.css"
+  },
+  {
+    "revision": "c44e4504459f7d9c2d46",
+    "url": "/twitter_clone/static/js/2.1e53df67.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/twitter_clone/static/js/2.1e53df67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43efb1d0e23f45644511",
+    "url": "/twitter_clone/static/js/main.87e16335.chunk.js"
+  },
+  {
+    "revision": "4ade550444ea6196af11",
+    "url": "/twitter_clone/static/js/runtime-main.4dde12f7.js"
+  },
+  {
+    "revision": "bfb88c4351c312cf0cb0d06aee66e686",
+    "url": "/twitter_clone/static/media/Aasif.jpg.bfb88c43.jpg"
+  },
+  {
+    "revision": "977e5aa8cb3f1c136b8899604e3d13e6",
+    "url": "/twitter_clone/static/media/post.jpg.977e5aa8.jpeg"
+  }
+]);
