dc928cb1f0cd876f4edefa001764841193e29fbb
Aasif Sheikh <Aasifsheikh820@gmail.com>
2020-10-25 13:38:11 +0530

    my second commit

---
+++ .history/src/Posts_20201024093724.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  FaComment,
+  FaChartBar,
+  FaLeaf,
+  FaHeart,
+  FaRegCheckCircle,
+} from "react-icons/fa";
+import Aasif from "./Image/Aasif.jpg.jpg";
+import Post from "./Image/post.jpg.jpeg";
+
+const Posts = () => {
+  return (
+    <div className="post">
+      <div className="post_first">
+        <div className="createImage">
+          <img src={Aasif} alt="" />
+        </div>
+        <div className="Firstname">
+          <strong>aaaaaasifsheikh</strong>
+          <FaRegCheckCircle className="verify" />
+        </div>
+        <div className="FirstUsername">
+          @Aaasifsheikh820 <span>6m.ago</span>
+        </div>
+      </div>
+      <div className="post_details">
+        <div className="post_details_msg">
+          lReprehenderit aute officia sit sint id tempor laboris aliqua. Et
+          minim reprehenderit elit ex ut adipisicing ea est dolore duis.
+          Consequat mollit tempor sit exercitation est nisi. Dolor eu sit dolor.
+        </div>
+        <div className="post_details_img">
+          <img src={Post} alt="post_img" />
+        </div>
+        <div className="reaction">
+          <span>
+            {" "}
+            <FaComment className="re" />
+            200
+          </span>
+          <span>
+            {" "}
+            <FaChartBar className="re" />
+            300
+          </span>
+          <span>
+            {" "}
+            <FaHeart className="re" />
+            100
+          </span>
+          <span>
+            {" "}
+            <FaLeaf className="re" />
+            500
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
---
+++ .history/src/TrendsKeyword_20201025065429.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const TrendsKeyword = () => {
+  const [state, setState] = useState([
+    {
+      id: 1,
+      country: "Trends In India",
+      keyword: "#Increase_In_Covid_Cases",
+      totalkeywords: "3444k",
+    },
+    {
+      id: 2,
+      country: "Trends In India",
+      keyword: "Know Your Nation",
+      totalkeywords: "3554k",
+    },
+    {
+      id: 3,
+      country: "Trends In India",
+      keyword: "Co-vacsean In India ",
+      totalkeywords: "56535k",
+    },
+    {
+      id: 4,
+      country: "Trends In India",
+      keyword: "Terror Attack",
+      totalkeywords: "4335k",
+    },
+  ]);
+  return (
+    <div className="keywords">
+      <div className="key">
+        <div className="keyword_heading">
+          <h4>Trends for you</h4>
+        </div>
+        {state.map((keyword) => (
+          <div key={keyword.id}>
+            <div className="country">{keyword.country}</div>
+            <div className="keyword_name">
+              <strong>{keyword.keyword}</strong>
+            </div>
+            <div className="keyword_tweets">{keyword.totalkeywords}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendsKeyword;
